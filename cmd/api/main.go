280cb3887a40f9d8449c5eb6b7a5de9e0ecb633b
Andrey <scythejudge@MacBook-Pro-Andrey.local>
2026-09-15 20:51:06 +0300

    0-2 - added os arg

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-var version = "0.1"
+var version = "0.2"
 
 func main() {
 	fmt.Println("Notifier Version:", version)
+
+	if len(os.Args) > 1 {
+		fmt.Printf("Notification for %s", os.Args[1])
+	} else {
+		fmt.Fprintln(os.Stderr, "You must provide at least 1 argument to successfully execute the program")
+		os.Exit(2)
+	}
+
 }
